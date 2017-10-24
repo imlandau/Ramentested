6d84fef9e4ca88b2d2a439847cbf7605b6a8f00f
imlandau <30660654+imlandau@users.noreply.github.com>
2017-10-24 11:00:47 -0400

    tested

---
+++ app/src/main/java/com/example/maste/ramen/createYourOwn.java
@@ -0,0 +1,13 @@
+package com.example.maste.ramen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class createYourOwn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_your_own);
+    }
+}
