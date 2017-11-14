63d97d711f1eb16ecf92c6ccbc1ea322a9ec9b52
imlandau <30660654+imlandau@users.noreply.github.com>
2017-11-14 09:56:41 -0500

    with fatimah code

--- app/src/main/java/com/example/maste/ramen/Home.java
+++ app/src/main/java/com/example/maste/ramen/Home.java
@@ -5,13 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+    Button login;
+    EditText username, password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        login=(Button)findViewById(R.id.login);
+        username=(EditText)findViewById(R.id.userName);
+        password=(EditText)findViewById(R.id.userPass);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+
+            }
+        });
+
 
         Button menuBtn = (Button) findViewById(R.id.menuBtn);
 
@@ -21,5 +37,16 @@ public class Home extends AppCompatActivity {
                 startActivity(new Intent(Home.this, MainActivity.class));
             }
         });
+    }
+    public void login() {
+        String user = username.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        if (user.equals("ramen") && pass.equals("ramen"))
+        {
+            Toast.makeText(this, "username and password matched!", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "username and password do no matched", Toast.LENGTH_LONG).show();
+        }
     }
 }
