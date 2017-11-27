848533b1c611a97a92e48b5b4cc348b157b20016
SuxyShellSnarf <mparker95@yahoo.com>
2017-11-26 22:30:01 -0500

    Cart functional.

--- app/src/main/java/com/example/maste/ramen/MainActivity.java
+++ app/src/main/java/com/example/maste/ramen/MainActivity.java
@@ -24,8 +24,15 @@ public class MainActivity extends AppCompatActivity {
         ImageButton dish4ImgBtn = (ImageButton) findViewById(R.id.dish4ImgBtn);
         ImageButton dish5ImgBtn = (ImageButton) findViewById(R.id.dish5ImgBtn);
 
+        final int[] cartHolder = new int[7];
+        cartHolder[1] = Cart.cart1.getQuantity();
+        cartHolder[2] = Cart.cart2.getQuantity();
+        cartHolder[3] = Cart.cart3.getQuantity();
+        cartHolder[4] = Cart.cart4.getQuantity();
+        cartHolder[5] = Cart.cart5.getQuantity();
+        cartHolder[6] = Cart.cart6.getQuantity();
 
-        Button cartBtn = (Button) findViewById(R.id.cartBtn);
+        final Button cartBtn = (Button) findViewById(R.id.cartBtn);
 
         Button homeTest = (Button) findViewById(R.id.homeTest);
 
@@ -41,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         cartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Cart.class));
+                startActivity(new Intent(MainActivity.this, CartActivity.class));
             }
         }); // end cartBtn OnClickListener
 
@@ -51,5 +58,50 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Home.class));
             }
         }); // end homeTest OnClickListener
+
+        dish1ImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                cartHolder[1]++;
+                Cart.cart1.setQuantity(cartHolder[1]);
+            }
+
+        });
+
+        dish2ImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                cartHolder[2]++;
+                Cart.cart2.setQuantity(cartHolder[2]);
+            }
+
+        });
+
+        dish3ImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                cartHolder[3]++;
+                Cart.cart3.setQuantity(cartHolder[3]);
+            }
+
+        });
+
+        dish4ImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                cartHolder[4]++;
+                Cart.cart4.setQuantity(cartHolder[4]);
+            }
+
+        });
+
+        dish5ImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                cartHolder[5]++;
+                Cart.cart5.setQuantity(cartHolder[5]);
+            }
+
+        });
     }
 }
