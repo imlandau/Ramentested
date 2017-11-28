d1319e675ad45c59faada1cb80f5283fb530b7d8
imlandau <30660654+imlandau@users.noreply.github.com>
2017-11-28 11:40:40 -0500

    all pages access

--- app/src/main/java/com/example/maste/ramen/SignUp.java
+++ app/src/main/java/com/example/maste/ramen/SignUp.java
@@ -1,7 +1,10 @@
 package com.example.maste.ramen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button homeBtn2 = (Button) findViewById(R.id.homeBtn2);
+        homeBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUp.this, Home.class));
+            }
+        });
+
     }
+
+
+
 }
